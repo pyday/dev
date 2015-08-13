204920d4fc3febd9fbe942ac1c0f110e3a239588
doracl <wcl_orange@163.com>
2015-08-13 13:20:43 +0800

    commit by wangcl

--- thread.py
+++ thread.py
@@ -3,16 +3,22 @@ __author__ = 'wangcl'
 from multiprocessing import Pool
 import time
 
+
 def f(x):
     time.sleep(1)
     return "wangcl"
+
 
 def callback(x):
     print x
     print "callback"
 
+
 if __name__ == '__main__':
-    pool = Pool(processes=1)              # Start a worker processes.
-    result = pool.apply_async(f, [10], callback=None) # Evaluate "f(10)" asynchronously calling callback when finished.
+    pool = Pool(processes=1)  # Start a worker processes.
+    result = pool.apply_async(f, [10], callback=None)  # Evaluate "f(10)" asynchronously calling callback when finished.
     result.wait()
     print "over"
+
+
+
