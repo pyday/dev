c66de0f058176edab0c42b229ff4479a99cd069f
doracl <wcl_orange@163.com>
2015-08-13 14:54:57 +0800

    commit by wangcl

--- log_ext/__init__.py
+++ log_ext/__init__.py
@@ -1,14 +1,14 @@
 __author__ = 'wangcl'
 
 import logging
+from logging.handlers import RotatingFileHandler
 import os
-
 
 
 def getLogger(name, level=logging.DEBUG):
     logger = logging.getLogger(name)
     path = os.path.join(os.path.dirname(__file__), "../logs/" + name + ".log")
-    hdlr = logging.FileHandler(path)
+    hdlr = RotatingFileHandler(path, maxBytes=5 * 1024 * 1024, backupCount=2)
     formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
     hdlr.setFormatter(formatter)
     logger.addHandler(hdlr)
